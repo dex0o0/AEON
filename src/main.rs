b51a347ab10852b65e034022180a53db35e16de9
dex0o0 <hassan13854444@gmail.com>
2026-04-10 16:51:32 +0330

    fix lot of warning

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,9 @@ mod modules{
     pub mod monitoring;
 }
 use daemon::daemon::*;
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
-use std::str::FromStr;
 use std::{io, u64};
 use std::thread;
 use std::time::Duration;
-
-use crate::daemon::log::Log;
 use crate::modules::monitoring;
 
 const FILE_CONF:&str="/tmp/data.json";
@@ -32,25 +26,25 @@ async fn run() -> io::Result<()>{
            monitoring::monswap();
            monitoring::moncpu().await;
            monitoring::gpu().await;
-           thread::sleep(Duration::from_secs(1));
+           thread::sleep(Duration::from_millis(200));
        }
    });
-   let _conf = tokio::spawn(async{
-        loop {
-            let path = PathBuf::from_str(FILE_CONF).expect("Error");
-            let path2 = PathBuf::from_str("/tmp/d.json").expect("Error");
-            let data = read_file(&path).expect("Error");
-            let _ = Log::save_log("FILE_CONF", format!("data:{}",data));
-            
-            let mut file = File::create(&path2).expect("FILE");
-            file.write_all(data.as_bytes()).expect("E");
-            thread::sleep(Duration::from_secs(5));
-        }
-   });
+   // let _conf = tokio::spawn(async{
+   //      loop {
+   //          let path = PathBuf::from_str(FILE_CONF).expect("Error");
+   //          let path2 = PathBuf::from_str("/tmp/d.json").expect("Error");
+   //          let data = read_file(&path).expect("Error");
+   //          let _ = Log::save_log("FILE_CONF", format!("data:{}",data));
+   //
+   //          let mut file = File::create(&path2).expect("FILE");
+   //          file.write_all(data.as_bytes()).expect("E");
+   //          thread::sleep(Duration::from_secs(5));
+   //      }
+   // });
    let _net_handle=tokio::spawn(async {
        loop{
             let _ = check_net().await;
-            thread::sleep(Duration::from_secs(60));
+            thread::sleep(Duration::from_secs(30));
        }
    });
    let _ =tokio::time::sleep(Duration::from_secs(u64::MAX)).await;
