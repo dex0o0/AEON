25f15ef59e1a12af7b4b929d21fa44e0dc532021
dex0o0 <hassan13854444@gmail.com>
2026-04-10 16:50:01 +0330

    fix lot of warning

--- src/daemon/daemon.rs
+++ src/daemon/daemon.rs
@@ -1,13 +1,10 @@
 use std::{
-    fs, io::{self, Read}, path::PathBuf, process::Command, sync::atomic::{AtomicBool,Ordering}
+    io,process::Command, sync::atomic::{AtomicBool,Ordering}
 
 };
-use crate::{daemon::{log::{self, Log}, notif::Notif}, modules::monitoring};
-
-
+use crate::{daemon::{log,notif::Notif},};
 
 static NETWORK_IS_UP:AtomicBool=AtomicBool::new(true);
-
 
 pub async fn check_net()-> io::Result<()>{
     let ping = Command::new("ping")
@@ -19,7 +16,6 @@ pub async fn check_net()-> io::Result<()>{
             
             if output.status.success(){
                 //net up 
-                //
                 //check status
                 if !was_net_up{
                     let _ = Notif::send("NETWORK", "back to online".to_string());
@@ -28,7 +24,6 @@ pub async fn check_net()-> io::Result<()>{
                 }
             }else {
                 //net down
-                //
                 //check status
                 if was_net_up{
                     let _ = Notif::send("NETWORK", "network is down".to_string());
@@ -52,19 +47,19 @@ pub async fn check_net()-> io::Result<()>{
     Ok(())  
 }
 
-pub async fn file_chek(path:PathBuf)-> String{
-    read_file(&path).map(|data| data).expect("Err")
-}
-pub fn read_file(path:&PathBuf)-> Option<String>{
-    let mut file = fs::File::open(path).expect("Err");
-    let mut data = String::new();
-    if file.read_to_string(&mut data).is_ok(){
-        let json = serde_json::to_string_pretty(&data).expect("Error");
-        println!("json:{}",json);
-        Some(json)
-    }else {
-        None
-    }
-    
-
-}
+// pub async fn file_chek(path:PathBuf)-> String{
+//     read_file(&path).map(|data| data).expect("Err")
+// }
+// pub fn read_file(path:&PathBuf)-> Option<String>{
+//     let mut file = fs::File::open(path).expect("Err");
+//     let mut data = String::new();
+//     if file.read_to_string(&mut data).is_ok(){
+//         let json = serde_json::to_string_pretty(&data).expect("Error");
+//         println!("json:{}",json);
+//         Some(json)
+//     }else {
+//         None
+//     }
+//
+//
+// }
