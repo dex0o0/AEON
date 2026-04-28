494971cf5a69df7268c93088c5812eddd5097b20
dex0o0 <hassan13854444@gmail.com>
2026-04-28 18:01:57 +0330

    convert to comment bulshit code

--- src/daemon/core.rs
+++ src/daemon/core.rs
@@ -1,20 +1,20 @@
-#[allow(unused_imports)]
-use std::io::{Read, Write};
-#[allow(unused_imports)]
-use std::net::TcpStream;
-
-
-pub fn test() -> Result<(), Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect("127.0.0.1:3001")?;  // پورت رو عوض کن
-    let request = "GET /status HTTP/1.1\r\nHost: localhost\r\n\r\n";
-    stream.write_all(request.as_bytes())?;
-    
-    let mut response = String::new();
-    stream.read_to_string(&mut response)?;
-    
-    println!("Response:\n{:?}", response);  // با println چاپ کن
-    // اگه می‌خوای با dbg ببینی:
-    // dbg!(&response);
-    
-    Ok(())
-}
+// #[allow(unused_imports)]
+// use std::io::{Read, Write};
+// #[allow(unused_imports)]
+// use std::net::TcpStream;
+//
+//
+// pub fn test() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut stream = TcpStream::connect("127.0.0.1:3001")?;  // پورت رو عوض کن
+//     let request = "GET /status HTTP/1.1\r\nHost: localhost\r\n\r\n";
+//     stream.write_all(request.as_bytes())?;
+//
+//     let mut response = String::new();
+//     stream.read_to_string(&mut response)?;
+//
+//     println!("Response:\n{:?}", response);  // با println چاپ کن
+//     // اگه می‌خوای با dbg ببینی:
+//     // dbg!(&response);
+//
+//     Ok(())
+// }
