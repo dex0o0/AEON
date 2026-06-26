3b518b681937375a42e292f4effe2dcf163ea1b0
dex0o0 <hassan13854444@gmail.com>
2026-06-26 18:04:14 +0330

    rewrite scan with netstate2

--- src/port/scan.rs
+++ src/port/scan.rs
@@ -1,4 +1,5 @@
 use local_ip_address::local_ip;
+use netstat2::*;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::net::{IpAddr, Ipv4Addr, TcpStream};
 use std::time::Duration;
@@ -33,39 +34,65 @@ pub fn scan_renge(addr: String, sp: u16, ep: u16) -> Vec<u16> {
     open_port
 }
 
+//Scan TCP Ports with netstat2
+pub fn scan_netstate_tcp() -> Vec<String> {
+    let mut out_list = vec![];
+
+    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let proto_flags = ProtocolFlags::TCP;
+    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
+
+    for si in sockets_info {
+        if let ProtocolSocketInfo::Tcp(tcp_si) = si.protocol_socket_info {
+            let data = format!(
+                "TCP {}:{} -> {}:{} {:?} - {}",
+                tcp_si.local_addr,
+                tcp_si.local_port,
+                tcp_si.remote_addr,
+                tcp_si.remote_port,
+                si.associated_pids,
+                tcp_si.state
+            );
+            out_list.push(data);
+        }
+    }
+    out_list
+}
+
+//Scan UDP Porst with netstat2
+pub fn scan_netstate_udp() -> Vec<String> {
+    let mut out_list = vec![];
+    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let proto_flags = ProtocolFlags::UDP;
+    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap();
+    for si in sockets_info {
+        if let ProtocolSocketInfo::Udp(udp_si) = si.protocol_socket_info {
+            let data = format!(
+                "UDP {}:{} -> *:* {:?}",
+                udp_si.local_addr, udp_si.local_port, si.associated_pids
+            );
+            out_list.push(data);
+        }
+    }
+    out_list
+}
+
 #[cfg(test)]
 mod test {
-    use super::*;
-    use std::net::TcpListener;
+    use crate::port::scan::{scan_netstate_tcp, scan_netstate_udp};
 
     #[test]
-    fn test_scan_loop_back_ports() {
-        let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-        let test_port = 1500;
-
-        let _listener =
-            TcpListener::bind(format!("{}:{}", ip, test_port)).expect("Failed to bind test port");
-
-        let open_ports = scan_renge("127.0.0.1".into(), 1u16, 12000u16);
-        println!("Open ports found: {:?}", open_ports);
-
-        assert!(
-            open_ports.contains(&test_port),
-            "Scanner missed the open port on local IP!"
-        );
+    fn test_tcp() {
+        let status = scan_netstate_tcp();
+        status.iter().for_each(|p| {
+            println!("{:?}", p);
+        });
     }
     #[test]
-    fn scan_on_local_ports() {
-        let ip = local_ip_address::local_ip().unwrap_or(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
-        let test_port = 1500;
-
-        let _listener =
-            TcpListener::bind(format!("{}:{}", ip, test_port)).expect("Failed to bind test port");
-        let open_port = sapl();
-        println!("{:?}", open_port);
-        assert!(
-            open_port.contains(&test_port),
-            "Scanner missed the open port on local IP!"
-        );
+    fn test_udp() {
+        let status = scan_netstate_udp();
+        status.iter().for_each(|p| {
+            println!("{:?}", p);
+        });
     }
 }
